3a96e142b1b24f662952e52c02cc807f9bf05e2e
Milagros <mirakel1401@gmail.com>
2020-07-01 22:25:37 -0500

    agregando detalle curso

--- aplicaciones/cursos/urls.py
+++ aplicaciones/cursos/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     re_path(r'^$', Curso.as_view(), name='p_inicio'),
+    re_path(r'^detalle_curso/(?P<slug>[^/]+)/$', CourseDetailView.as_view(), name='detalle_curso'),
 ]
--- aplicaciones/cursos/views.py
+++ aplicaciones/cursos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404
 from .models import Curso as mis_cursos
 # Create your views here.
 
@@ -21,4 +23,18 @@ class Index_principal(TemplateView):
         context = super(Index_principal,
                         self).get_context_data(**kwargs)
         context['cursos'] = mis_cursos.objects.filter(estado=True)
+        return context
+
+
+class CourseDetailView(DetailView):
+    template_name = 'cursos/detalle_curso.html'
+
+    def get_queryset(self):
+        self.name = get_object_or_404(mis_cursos, slug=self.kwargs['slug'])
+        return mis_cursos.objects.filter(nombre=self.name)
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView,
+                        self).get_context_data(**kwargs)
+        context['curso'] = mis_cursos.objects.get(nombre=self.name)
         return context
